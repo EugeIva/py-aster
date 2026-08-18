39795aceb899a57c83be4d888f27adc91af41e98
eugene <eivanovdev@pm.me>
2026-08-17 23:56:04 -0300

    player controls

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
+
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,13 +10,27 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    clock = pygame.time.Clock()
+    dt = 0.0
+    # updatable = pygame.sprite.Group()
+    # drawable = pygame.sprite.Group()
+    # Player.containers = (updatable, drawable)
 
     while True:
+        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        # updatable.update(dt)
+        # for d in drawable:
+        #     d.draw(screen)
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
